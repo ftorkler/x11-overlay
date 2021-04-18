5d2a7650f59c97d895e685d39aecdf28e8b68a76
Florian Torkler <florian.torkler@gmx.net>
2021-04-18 22:07:06 +0200

    handle sigterm signal and destroy window gracefully

--- src/overlay.cpp
+++ src/overlay.cpp
@@ -1,16 +1,37 @@
 #include <chrono>
+#include <signal.h>
 #include <thread>
 
 #include "window.h"
 
+bool running = true;
+
+void sigHandler(int signum)
+{
+    running = false;
+}
+
+void catchSigterm()
+{
+   struct sigaction sigIntHandler;
+
+   sigIntHandler.sa_handler = sigHandler;
+   sigemptyset(&sigIntHandler.sa_mask);
+   sigIntHandler.sa_flags = 0;
+
+   sigaction(SIGINT, &sigIntHandler, nullptr);
+}
+
 int main(int argc, char *argv[])
 {
+    catchSigterm();
+
     X11Window* window = new X11Window(100, 100, 480, 640);
 
     XColor bgColor = window->createColor(0, 0, 0, 100);
     XColor redColor = window->createColor(255, 0, 0, 200);
 
-    while (true)
+    while (running)
     {
         window->clear();
         window->drawRect(0, 0, 350, 30, bgColor);
@@ -21,6 +42,8 @@ int main(int argc, char *argv[])
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+    delete window;
 
     return 0;
 }
